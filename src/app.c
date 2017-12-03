195b469250d84605e2b2265e24e40495a976650f
WareAnj <ware.angelicap@gmail.com>
2017-12-04 00:46:54 +0800

    Removed playable code

--- src/app.c
+++ src/app.c
@@ -30,28 +30,7 @@ int main() {
     int direction, face;
     
     readFile(rubixcube);
-
-    while (1) {
-
-        print(rubixcube);
-
-        printf("\n");
-        printf("Rotate Side (0-5): ");
-        scanf("%d", & face);
-
-        printf("1-CW or 2-CCW: ");
-        scanf("%d", & direction);
-
-        if (direction == 1) {
-            rotateCube(rubixcube, face, "CLOCKWISE");
-            printf("\n\t\tCLOCKWISE\n");
-        } else if (direction == 2) {
-            rotateCube(rubixcube, face, "!CLOCKWISE");
-            printf("\n\t\tCOUNTER-CLOCKWISE\n");
-        } else printf("No direction chosen! \n");
-        
-
+    if (!check(rubixcube)) {
+        solve();
     }
-
-
 }
