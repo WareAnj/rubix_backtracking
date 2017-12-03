006994d7a23a31875204ee5bf49b1b9d5a6d74c5
WareAnj <ware.angelicap@gmail.com>
2017-12-04 00:01:28 +0800

    Updated shell runner

--- .app
+++ .app
@@ -1,3 +1,14 @@
 reset
-gcc src/app.c -o out/app.out
-./out/app.out
+rm out/app.out
+cd src
+gcc app.c -o app.out
+if [ $? -eq 0 ]; then
+    mv app.out ../out/app.out
+    cd ../out
+    ./app.out
+    echo [OK]
+else
+    rm app.out
+    cd ..
+    echo [FAIL]
+fi
--- src/backtracking/read_file.h
+++ src/backtracking/read_file.h
@@ -1,43 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <time.h>
 
-#include "backtracking/print.h"
-#include "backtracking/convert.h"
-#include "backtracking/rotate_cube.h"
-#include "backtracking/check.h"
+#include "convert.h"
 
-#define N 6 //min number of steps
-#define faces 6
-#define row 3
-#define col 3
-#define true 1
-#define false 0
-int i, j, k, candidate;
-/** 
- * GROUP Y - CMSC 142 C-2L
- * 
- * @AUTHOR: [
- *  Menguito, Aleck,
- *  Milano, Angelo,
- *  Ware, Angelica
- * ]
- **/
-
-int main() {
-
-    int rubixcube[6][3][3]; //3D array
-    int i = 0, j = 0, k = 0, direction, face;
+int read_file () {
+    int rubixcube[6][3][3];
+    int i = 0, j = 0, k = 0;
     char temp1, temp2, temp3;
     FILE * fp;
 
-    fp = fopen("../res/input.txt", "r");
-
+    fp = fopen("../../res/input.txt", "r");
 
     while (!feof(fp)) {
-
-
         if (i == 0) {
             for (j = 0; j < 3; j++) {
                 fscanf(fp, "%c%c%c\n", & temp1, & temp2, & temp3);
@@ -131,28 +105,5 @@ int main() {
 
     fclose(fp);
 
-
-    while (1) {
-
-        print(rubixcube);
-
-        printf("\n");
-        printf("Rotate Side (0-5): ");
-        scanf("%d", & face);
-
-        printf("1-CW or 2-CCW: ");
-        scanf("%d", & direction);
-
-        if (direction == 1) {
-            rotate_cube(rubixcube, face, "CLOCKWISE");
-            printf("\n\t\tCLOCKWISE\n");
-        } else if (direction == 2) {
-            rotate_cube(rubixcube, face, "!CLOCKWISE");
-            printf("\n\t\tCOUNTER-CLOCKWISE\n");
-        } else printf("No direction chosen! \n");
-        
-
-    }
-
-
+    return rubixcube;
 }
