4b37c1bbb6e0ee8dd8a901f88a737b5d98962c7c
WareAnj <ware.angelicap@gmail.com>
2017-12-04 00:30:25 +0800

    Optimized file reading

--- src/backtracking/read_file.h
+++ src/backtracking/read_file.h
@@ -10,95 +10,18 @@ void read_file (int rubixcube[6][3][3]) {
 
     fp = fopen("../res/input.txt", "r");
     while (!feof(fp)) {
-        if (i == 0) {
-            for (j = 0; j < 3; j++) {
-                fscanf(fp, "%c%c%c\n", & temp1, & temp2, & temp3);
+        for (j = 0; j < 3; j++) {
+            fscanf(fp, "%c%c%c\n", & temp1, & temp2, & temp3);
 
-                rubixcube[i][j][0] = (int) temp1;
-                rubixcube[i][j][1] = (int) temp2;
-                rubixcube[i][j][2] = (int) temp3;
+            rubixcube[i][j][0] = (int) temp1;
+            rubixcube[i][j][1] = (int) temp2;
+            rubixcube[i][j][2] = (int) temp3;
 
-                convert( & rubixcube[i][j][0]);
-                convert( & rubixcube[i][j][1]);
-                convert( & rubixcube[i][j][2]);
-            }
-            i++;
+            convert( & rubixcube[i][j][0]);
+            convert( & rubixcube[i][j][1]);
+            convert( & rubixcube[i][j][2]);
         }
-        if (i == 1) {
-            for (j = 0; j < 3; j++) {
-                fscanf(fp, "%c%c%c\n", & temp1, & temp2, & temp3);
-
-                rubixcube[i][j][0] = (int) temp1;
-                rubixcube[i][j][1] = (int) temp2;
-                rubixcube[i][j][2] = (int) temp3;
-
-                convert( & rubixcube[i][j][0]);
-                convert( & rubixcube[i][j][1]);
-                convert( & rubixcube[i][j][2]);
-            }
-            i++;
-
-        }
-        if (i == 2) {
-            for (j = 0; j < 3; j++) {
-                fscanf(fp, "%c%c%c\n", & temp1, & temp2, & temp3);
-
-                rubixcube[i][j][0] = (int) temp1;
-                rubixcube[i][j][1] = (int) temp2;
-                rubixcube[i][j][2] = (int) temp3;
-
-                convert( & rubixcube[i][j][0]);
-                convert( & rubixcube[i][j][1]);
-                convert( & rubixcube[i][j][2]);
-            }
-            i++;
-
-        }
-        if (i == 3) {
-            for (j = 0; j < 3; j++) {
-                fscanf(fp, "%c%c%c\n", & temp1, & temp2, & temp3);
-
-                rubixcube[i][j][0] = (int) temp1;
-                rubixcube[i][j][1] = (int) temp2;
-                rubixcube[i][j][2] = (int) temp3;
-
-                convert( & rubixcube[i][j][0]);
-                convert( & rubixcube[i][j][1]);
-                convert( & rubixcube[i][j][2]);
-            }
-            i++;
-
-        }
-        if (i == 4) {
-            for (j = 0; j < 3; j++) {
-                fscanf(fp, "%c%c%c\n", & temp1, & temp2, & temp3);
-
-                rubixcube[i][j][0] = (int) temp1;
-                rubixcube[i][j][1] = (int) temp2;
-                rubixcube[i][j][2] = (int) temp3;
-
-                convert( & rubixcube[i][j][0]);
-                convert( & rubixcube[i][j][1]);
-                convert( & rubixcube[i][j][2]);
-            }
-            i++;
-
-        }
-        if (i == 5) {
-            for (j = 0; j < 3; j++) {
-                fscanf(fp, "%c%c%c\n", & temp1, & temp2, & temp3);
-
-                rubixcube[i][j][0] = (int) temp1;
-                rubixcube[i][j][1] = (int) temp2;
-                rubixcube[i][j][2] = (int) temp3;
-
-                convert( & rubixcube[i][j][0]);
-                convert( & rubixcube[i][j][1]);
-                convert( & rubixcube[i][j][2]);
-            }
-            i++;
-
-        }
+        i++;
     }
 
     fclose(fp);
